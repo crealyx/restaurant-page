c276f5b9091eb46ba6d419e44ca73ee1cd0133a6
crealyx <sanberk5416@hotmail.com>
2021-05-14 20:54:43 +0300

    Initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.config.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+});
---
+++ webpack.config.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.config.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "[name].css" })],
+});
